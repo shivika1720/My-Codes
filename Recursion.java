6eb8a670f80b7638f90470a912ba5494c2b5a87e
Shivika Kejriwal <59910256+shivika1720@users.noreply.github.com>
2021-03-19 23:43:49 +0530

    Factorial code using recursion

--- Recursion.java
+++ Recursion.java
@@ -1,17 +1,18 @@
-public class Recursion { // Find the factorial of a number using recursion
+  public class Recursion     // Find the factorial of a number using recursion
+ { 
 	
 	public static int factorial(int n)  // method function
 	{ 
 	
-	 if(n<=1)   //defining conditions
+	   if(n<=1)   //defining conditions
 		return 1;
-	 else 
+	   else 
 		 return n*factorial(n-1);
 	}
    
-  public static void main(String[] args) {   // Main
-		
-      System.out.println("The factorial is " +factorial(6));
+        public static void main(String[] args)   // Main
+        {   
+	   System.out.println("The factorial is " +factorial(6));
 	}
 
 }
